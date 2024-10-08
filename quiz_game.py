05ca5d1ebcdab97abff7efef45ded253b1b41778
Roberson Robert <robersonrobert@Robersons-MacBook-Air-1108.local>
2024-10-07 22:29:58 -0400

    Added multiple questions and logic to my game

--- quiz_game.py
+++ quiz_game.py
@@ -1,1 +1,37 @@
-quiz_game
+print("Welcome to Rob's Quiz Game!")
+print("This is a Computer Knowledge Quiz Game!")
+
+playing = input("Do you want to play? ")
+
+if playing.lower() != "yes":
+    quit()
+
+print("Okay! Let's play :) ")
+
+#Question 1
+answer = input("What does CPU stand for? ")
+if answer == "central processing unit":
+    print("Correct!")
+else:
+    print("Incorrect!")
+
+#Question 2
+answer = input("What does GPU stand for? ")
+if answer == "graphics processing unit":
+    print("Correct!")
+else:
+    print("Incorrect!")
+
+#Question 3
+answer = input("What does RAM stand for? ")
+if answer == "random access memory":
+    print("Correct!")
+else:
+    print("Incorrect!")
+
+#Question 4
+answer = input("What does LCD stand for? ")
+if answer == "liquid crystal display":
+    print("Correct!")
+else:
+    print("Incorrect!")
